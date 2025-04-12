010606e1fd61ba46562fa1a1dd0f58702c120711
Tsedniya Frezewed <ktsedniya@gmail.com>
2025-04-12 12:42:15 +0400

    done is done

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menu = document.querySelector("#menu");
+    const nav = document.querySelector(".links");
+
+    menu.onclick = () => {
+        nav.classList.toggle('active'); 
+    };
+});
