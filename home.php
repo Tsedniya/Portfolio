8d530f4e2e275096a3be5bd8dbf6b289eadb16a5
Tsedniya Frezewed <ktsedniya@gmail.com>
2025-05-02 00:43:52 +0400

    started

--- home.php
+++
@@ -1,9 +0,0 @@
-<section id="home">
-        <img src="tsedi.jpg" alt="image of Tsedniya">
-
-        <div class="info-box">
-            <h1><span>Hello, I'm Tsedniya</span></h1>
-            <h3><span>Frontend Developer</span></h3>
-            <p>I’m Tsedniya, a passionate and innovative front-end developer with over six months of experience crafting beautiful, user-friendly, and responsive web interfaces. My goal is to transform your ideas into engaging and seamless digital experiences.</p>
-        </div>
-    </section>
